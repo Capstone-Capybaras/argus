0a117b78913060d84e912cc55316ef8eb6639169
Joel Tay <joeltwh1999@gmail.com>
2024-10-17 22:52:03 +0800

    sanity check for config files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,15 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import * as path from 'path';
+import * as fs from 'fs';
+
+console.log(`NODE_ENV sanity check: ${process.env.NODE_ENV}`);
+console.log(
+  'env file exists:',
+  fs.existsSync(
+    path.join(__dirname, '..', '..', `.env.${process.env.NODE_ENV}`),
+  ),
+);
 
 @Module({
   imports: [
