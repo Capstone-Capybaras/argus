29ad06247edfe40ded1a3032becbf86d74361e50
Joel Tay <joeltwh1999@gmail.com>
2024-10-23 14:12:57 +0800

    remove configmodule import because it is imported globally

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.registerAsync({
-      imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.getOrThrow('JWT_SECRET'),
         signOptions: { expiresIn: '60s' },
