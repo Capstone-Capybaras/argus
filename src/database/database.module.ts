e3c39ccf18625aa9b289a6730676c722d5ceb0eb
Ankita Parashar <ankitap2001.ap@gmail.com>
2024-10-23 12:31:34 +0800

    first commit

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -14,6 +14,7 @@ import * as schemas from './schema';
           connectionString: configService.getOrThrow('DATABASE_URL'),
         });
         // validate db connection
+        // comment to check if its working
         try {
           const client = await pool.connect();
           client.release();
