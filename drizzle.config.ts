09001feebea95d1eb95d1b688b4533c561fa1910
atheNahh <athenachua27@gmail.com>
2024-10-23 12:10:38 +0800

    process env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   dialect: 'postgresql',
   schema: './src/database/schema.ts',
   dbCredentials: {
-    url: `.env.${process.env.DATABASE_URL}`,
+    url: process.env.DATABASE_URL,
   },
 });
