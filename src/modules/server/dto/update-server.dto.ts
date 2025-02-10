2a0184f6fb9137bd7ab45b8453da4deb1b5a1a06
Ankita Parashar <ankitap2001.ap@gmail.com>
2025-02-10 11:38:09 +0800

    deleted sender/server crud files

--- src/modules/server/dto/update-server.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, IsIn } from 'class-validator';
-import { serverTable } from 'src/database/schema';
-import { InferUpdate } from 'src/utils/modelToDtoTypes';
-
-export class UpdateServerDto implements InferUpdate<typeof serverTable> {
-
-  @IsIn(['simx1', 'simx2'])
-  server: 'simx1' | 'simx2';
-}
