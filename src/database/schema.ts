b658e5257d189b9feef80229967b1477e989ecbf
atheNahh <athenachua27@gmail.com>
2025-02-14 11:13:43 +0800

    added msel table for version control feature

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,3 +1,4 @@
+import { datetime } from 'drizzle-orm/mysql-core';
 import {
   date,
   pgTable,
@@ -165,6 +166,8 @@ export const injectsTable = pgTable(
   }),
 );
 
+
+
 export const responsesTable = pgTable('responses', {
   inject_id: varchar()
     .notNull()
@@ -330,3 +333,13 @@ export const serverTypeEnum = pgEnum('server_type', ['simx1', 'simx2']);
 export const serverTable = pgTable('server', {
   server: serverTypeEnum().default('simx1'),
 });
+
+export const mselTable = pgTable('msel',{
+  project_id: integer().notNull().references(()=>projectsTable.id),
+  msel: text().notNull(),
+  date_uploaded: timestamp().notNull(),
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.msel, table.project_id] }),
+  }),
+)
