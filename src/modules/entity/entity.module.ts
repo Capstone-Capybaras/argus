7c083bff98993c781544132a47310c1739a4b7cd
Joel Tay <joeltwh1999@gmail.com>
2025-02-19 12:08:42 +0800

    fix DI

--- src/modules/entity/entity.module.ts
+++ src/modules/entity/entity.module.ts
@@ -3,9 +3,10 @@ import { EntityService } from './entity.service';
 import { EntityController } from './entity.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { ParticipantsModule } from '../participants/participants.module';
+import { AssetsModule } from '../assets/assets.module';
 
 @Module({
-  imports: [DatabaseModule, ParticipantsModule],
+  imports: [DatabaseModule, ParticipantsModule, AssetsModule],
   providers: [EntityService],
   controllers: [EntityController],
 })
