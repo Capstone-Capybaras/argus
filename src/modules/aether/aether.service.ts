0936b2168a2cc5b0bac209ec6ee71b1cc32161ab
Joel Tay <joeltwh1999@gmail.com>
2025-02-21 12:17:19 +0800

    jwt and axios setup

--- src/modules/aether/aether.service.ts
+++ src/modules/aether/aether.service.ts
@@ -1,6 +1,22 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+interface AetherJwtPayload {
+  username: string;
+}
 
 @Injectable()
 export class AetherService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  private async createToken(): Promise<string> {
+    const payload: AetherJwtPayload = { username: 'argus' };
+    return this.jwtService.signAsync(payload);
+  }
+
   async generateScenario() {}
 }
