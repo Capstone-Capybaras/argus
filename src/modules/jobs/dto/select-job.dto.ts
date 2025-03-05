7c4074a61b22a370dfb307eac14d4c77a1a6f4b5
Joel Tay <joeltwh1999@gmail.com>
2025-03-05 13:00:12 +0800

    type enum for job dto

--- src/modules/jobs/dto/select-job.dto.ts
+++ src/modules/jobs/dto/select-job.dto.ts
@@ -1,10 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { jobsTable } from 'src/database/schema';
 import { InferSelect } from 'src/utils/modelToDtoTypes';
 
 export class SelectJobDto implements InferSelect<typeof jobsTable> {
   id: number;
+
+  @ApiProperty({ enum: ['scenario', 'msel', 'threat'] })
   type: 'scenario' | 'msel' | 'threat';
+
+  @ApiProperty({ enum: ['pending', 'failed', 'done'] })
   status: 'pending' | 'failed' | 'done';
+
   created_at: Date;
   name: string;
   project_id: number;
