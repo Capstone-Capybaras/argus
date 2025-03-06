aa461762f9b9436ade115ed3c7cbc246b9bd8589
Joel Tay <joeltwh1999@gmail.com>
2025-03-06 15:34:52 +0800

    lint and format

--- src/utils/uriDecode.pipe.ts
+++ src/utils/uriDecode.pipe.ts
@@ -1,8 +1,8 @@
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+import { PipeTransform, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UriDecodePipe implements PipeTransform<string, string> {
-  transform(value: string, metadata: ArgumentMetadata) {
+  transform(value: string) {
     return decodeURIComponent(value);
   }
 }
