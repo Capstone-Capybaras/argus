5e2a2108d380ea69319ce4c17fa6b5d951c3bcb3
atheNahh <athenachua27@gmail.com>
2025-03-07 15:45:26 +0800

    lint

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -125,7 +125,7 @@ export const threatLandscapeGeneratedTable = pgTable(
     opportunity: text(),
     opportunity_reason: text(),
     generation_inputs: json().notNull(),
-  }
+  },
 );
 
 export const ttpUsedTable = pgTable(
@@ -176,29 +176,26 @@ export const scenariosTable = pgTable(
 );
 
 // this must always be kept in sync with scenarios
-export const scenariosGeneratedTable = pgTable(
-  'scenarios_generated',
-  {
-    scenario_number: varchar().notNull(),
-    additional_context: text(),
-    scenario_title: text(),
-    threat_actor_motivation: text(),
-    intended_system_impact: text(),
-    intended_biz_impact: text(),
-    attack_sophistication: text(),
-    severity_level: text(),
-    initial_access: text(),
-    exploit: text(),
-    impact: text(),
-    project_id: integer()
-      .notNull()
-      .references(() => projectsTable.id, { onDelete: 'cascade' }),
-    asset_id: integer()
-      .notNull()
-      .references(() => assetsTable.id, { onDelete: 'cascade' }),
-    generation_inputs: json().notNull(),
-  }
-);
+export const scenariosGeneratedTable = pgTable('scenarios_generated', {
+  scenario_number: varchar().notNull(),
+  additional_context: text(),
+  scenario_title: text(),
+  threat_actor_motivation: text(),
+  intended_system_impact: text(),
+  intended_biz_impact: text(),
+  attack_sophistication: text(),
+  severity_level: text(),
+  initial_access: text(),
+  exploit: text(),
+  impact: text(),
+  project_id: integer()
+    .notNull()
+    .references(() => projectsTable.id, { onDelete: 'cascade' }),
+  asset_id: integer()
+    .notNull()
+    .references(() => assetsTable.id, { onDelete: 'cascade' }),
+  generation_inputs: json().notNull(),
+});
 
 // TODO: generated tables for threats
 
