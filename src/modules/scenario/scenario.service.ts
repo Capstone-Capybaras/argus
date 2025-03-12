301f4227ad0ee6abb1761ea4a66ce5554b5684ef
Joel Tay <joeltwh1999@gmail.com>
2025-03-12 21:40:30 +0800

    return scenarios and injects by scenario_number sort

--- src/modules/scenario/scenario.service.ts
+++ src/modules/scenario/scenario.service.ts
@@ -48,7 +48,10 @@ export class ScenarioService {
 
   // Retrieve all scenarios
   async getScenarios() {
-    const scenarios = await this.db.select().from(scenariosTable);
+    const scenarios = await this.db
+      .select()
+      .from(scenariosTable)
+      .orderBy(scenariosTable.scenario_number);
     return scenarios;
   }
 
@@ -116,6 +119,7 @@ export class ScenarioService {
     const rows = await this.db
       .select()
       .from(scenariosTable)
+      .orderBy(scenariosTable.scenario_number)
       .leftJoin(assetsTable, eq(scenariosTable.asset_id, assetsTable.id))
       .where(eq(scenariosTable.project_id, project_id));
 
